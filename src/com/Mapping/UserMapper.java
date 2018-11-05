737db9ba174c5921701db0fd048d6dc326d7d43b
YH1900 <yanzhang.163@qq.com>
2018-11-05 17:16:00 +0800

    测试

--- src/com/Mapping/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.Mapping;
-
-import com.pojo.User;
-
-public interface UserMapper {
-    public User findeUserById(int id) throws Exception;
-}
--- src/com/UserMapper/UserDao.java
+++
@@ -1,7 +0,0 @@
-package com.UserMapper;
-
-import com.pojo.User;
-
-public interface UserDao {
-    public User findeUserById(int id) throws Exception;
-}
