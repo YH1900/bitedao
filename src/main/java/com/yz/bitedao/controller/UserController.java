dc2b8e06d950216f472259ce4562c05734d6c405
YH1900 <yanzhang.163@qq.com>
2020-05-19 11:44:44 +0800

    修改controller

---
+++ src/main/java/com/yz/bitedao/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.yz.bitedao.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 2020/5/19
+ * 创建人:yz
+ */
+@RestController
+public class UserController {
+    @GetMapping("/test")
+    public String test(){
+        return  "test";
+    }
+}
