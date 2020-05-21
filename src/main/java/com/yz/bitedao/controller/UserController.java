55a2b964b290d28632b28acd869a24e2a50043de
YH1900 <yanzhang.163@qq.com>
2020-05-21 20:25:56 +0800

    修改controller1

--- src/main/java/com/yz/bitedao/controller/UserController.java
+++ src/main/java/com/yz/bitedao/controller/UserController.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @GetMapping("/test")
     public String test(){
-        return  "t1a2sdasasddt2";
+        return  "t1a2sdwerEasasddt2";
     }
 }
