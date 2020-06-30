e1b76f39c67a55dc3d05fdef2071fa9f0d2f9299
YH1900 <yanzhang.163@qq.com>
2020-06-30 19:25:04 +0800

    121212asdasd

--- src/main/java/com/yz/bitedao/controller/UserController.java
+++ src/main/java/com/yz/bitedao/controller/UserController.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @GetMapping("/test")
     public String test(){
-        return  "asd燕1121122樟";
+        return  "12";
     }
 }
