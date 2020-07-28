ba449b1078b11abc82375bd7befc5c8ef3b7a025
YH1900 <yanzhang.163@qq.com>
2020-07-28 14:06:59 +0800

    test

--- src/main/java/com/yz/bitedao/controller/UserController.java
+++ src/main/java/com/yz/bitedao/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.yz.bitedao.controller;
 
-import com.yz.bitedao.image.imageScan.util.TerminalUtil;
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,9 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class UserController {
+
     @GetMapping("/test")
     public String index(String test){
-        TerminalUtil.indexTest(test);
-        return "测 试 用 例。燕樟212";
+        System.out.println("CMDI");
+        String asdas="asdas";
+
+        System.out.println("CMDI");
+        return "";
     }
 }
